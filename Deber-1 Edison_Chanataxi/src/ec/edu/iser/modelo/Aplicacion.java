38d0e3c7ae60f3aa9b78e8eb9bbf48e7d94b1c35
edisonchanataxi <32962268+edisonchanataxi@users.noreply.github.com>
2018-01-30 00:37:02 -0500

    Add files via upload

---
+++ Deber-1 Edison_Chanataxi/src/ec/edu/iser/modelo/Aplicacion.java
@@ -0,0 +1,14 @@
+package ec.edu.iser.modelo;
+
+/**
+ *
+ * @author Andres
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        Lista l= new Lista();
+        //l.crearLista();
+        //l.imprimir();
+        l.menu();
+    }
+}
